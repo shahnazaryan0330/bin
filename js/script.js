0bdc62b0f0c775b8fdb63f34f0c52ae22c1a65dc
shahnazaryan0330 <shahnazaryan.0330@gmail.com>
2023-01-22 11:02:30 +0400

    2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-
-
-btn.addEventListener('click', () => {
+document.addEventListener('input', () => {
    const amount = +document.querySelector('#amountInput').value
    const purchase = +document.querySelector('#purchaseInput').value
    const sell = +document.querySelector('#sellInput').value
-   const btn = document.querySelector('#btn')
    const result = document.querySelector('.result')
 
    let temp = amount / purchase * sell
